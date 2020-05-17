3b5a0d46747d7c1e3d33c682279d12008e9e1905
andylwilson <andy787@gmail.com>
2020-05-16 19:40:06 -0700

    updates ui for account settings, adds manage recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,31 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home";
-import CreateRecipe from "./components/CreateRecipe";
-import AccountSettings from "./components/AccountSettings";
-import CustomNavigation from "./components/CustomNavigation";
-import FormLogin from "./components/FormLogin";
-import FormCreateAccount from "./components/CreateAccount";
-import RecoverPassword from "./components/RecoverPassword";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CreateRecipe from './components/CreateRecipe';
+import AccountSettings from './components/AccountSettings';
+import CustomNavigation from './components/CustomNavigation';
+import FormLogin from './components/FormLogin';
+import FormCreateAccount from './components/CreateAccount';
+import RecoverPassword from './components/RecoverPassword';
+import ManageRecipes from './components/ManageRecipes';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <CustomNavigation />
-        <Route exact path="/" component={Home} />
-        <Route path="/createrecipe" component={CreateRecipe} />
-        <Route exact path="/accountsettings" component={AccountSettings} />
-        <Route path="/formlogin" component={FormLogin} />
-        <Route path="/createaccount" component={FormCreateAccount} />
-        <Route path="/recoverpassword" component={RecoverPassword} />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<CustomNavigation />
+				<Route exact path="/" component={Home} />
+				<Route path="/createrecipe" component={CreateRecipe} />
+				<Route exact path="/accountsettings" component={AccountSettings} />
+				<Route path="/formlogin" component={FormLogin} />
+				<Route path="/createaccount" component={FormCreateAccount} />
+				<Route path="/recoverpassword" component={RecoverPassword} />
+				<Route path="/managerecipes" component={ManageRecipes} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
