6d734479443d762babe548951e1d52ec77a3db19
Pabriones <Pabriones@users.noreply.github.com>
2020-05-29 11:47:15 -0700

    Switch createaccount.js from class to cons etc

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,73 +1,136 @@
-import React, { Component } from "react";
-import { Jumbotron, Container, Form, Button, Col } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Jumbotron, Container, Form, Button, Col } from 'react-bootstrap';
 
-class FormCreateAccount extends Component {
-  render() {
-    return (
-      <Container>
-        <Jumbotron>
-          <h2>Create your Tasty Byte Account</h2>
-          <br />
-          <br />
+const CreateAccount = () => {
+	const [ formData, setFormData ] = useState({
+		firstname: '',
+		lastname: '',
+		username: '',
+		email: '',
+		password: '',
+		password2: ''
+	});
 
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter First name" />
-              </Form.Group>
+	const { firstname, lastname, username, email, password, password2 } = formData;
 
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter Last name" />
-              </Form.Group>
-            </Form.Row>
+	const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="user" placeholder="Enter username" />
-              </Form.Group>
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		if (password !== password2) {
+			console.log('Passwords do not match');
+		} else {
+			console.log('Success');
+		}
+	};
 
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-            </Form.Row>
+	return (
+		<Container>
+			<Jumbotron onSubmit={(e) => onSubmit(e)}>
+				<h2>Create your Tasty Byte Account</h2>
+				<br />
+				<br />
 
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" />
-              </Form.Group>
+				<Form>
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>First Name</Form.Label>
+							<Form.Control
+								type="text"
+								name="firstname"
+								value={firstname}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Enter First name"
+							/>
+						</Form.Group>
 
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Verify Password</Form.Label>
-                <Form.Control type="password" placeholder="Verify Password" />
-              </Form.Group>
-            </Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Last Name</Form.Label>
+							<Form.Control
+								type="text"
+								name="lastname"
+								value={lastname}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Enter Last name"
+							/>
+						</Form.Group>
+					</Form.Row>
 
-            <Form.File id="formcheck-api-regular">
-              <Form.File.Label>Upload Profile Picture</Form.File.Label>
-              <Form.File.Input />
-            </Form.File>
-            <br />
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Username</Form.Label>
+							<Form.Control
+								type="text"
+								name="username"
+								value={username}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Enter username"
+							/>
+						</Form.Group>
 
-            <Form.Group id="formGridCheckbox">
-              <Form.Check
-                type="checkbox"
-                label="I agree to terms and conditions."
-              />
-            </Form.Group>
+						<Form.Group as={Col}>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								type="email"
+								name="email"
+								value={email}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Enter email"
+							/>
+						</Form.Group>
+					</Form.Row>
 
-            <Button variant="primary" type="submit">
-              Create Account
-            </Button>
-          </Form>
-        </Jumbotron>
-      </Container>
-    );
-  }
-}
+					<Form.Row>
+						<Form.Group as={Col}>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type="password"
+								name="password"
+								minLength="6"
+								value={password}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Enter password"
+							/>
+						</Form.Group>
 
-export default FormCreateAccount;
+						<Form.Group as={Col}>
+							<Form.Label>Confirm Password</Form.Label>
+							<Form.Control
+								type="password"
+								name="password2"
+								value={password2}
+								onChange={(e) => onChange(e)}
+								required
+								placeholder="Verify Password"
+							/>
+						</Form.Group>
+					</Form.Row>
+
+					<Form.File id="formcheck-api-regular">
+						<Form.File.Label>Upload Profile Picture</Form.File.Label>
+						<Form.File.Input />
+					</Form.File>
+					<br />
+
+					<Form.Group id="formGridCheckbox">
+						<Form.Check type="checkbox" label="I agree to terms and conditions." />
+					</Form.Group>
+
+					<Button value="CreateAccount" type="submit" onSubmit={(e) => onSubmit(e)}>
+						Create Account
+					</Button>
+					<p>
+						Already have an account? <a href="/FormLogin">Sign in</a>
+					</p>
+				</Form>
+			</Jumbotron>
+		</Container>
+	);
+};
+
+export default CreateAccount;
--- client/src/components/FormLogin.js
+++ client/src/components/FormLogin.js
@@ -1,38 +1,64 @@
-import React, { Component } from "react";
-import { Jumbotron, Container, Form, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Jumbotron, Container, Form, Button } from 'react-bootstrap';
 
-class FormLogin extends Component {
-  render() {
-    return (
-      <Container>
-        <Jumbotron>
-          <h1 className="home-title">Welcome to TastyByte!</h1>
-          <h2>Login</h2>
-          <Form>
-            <Form.Group controlId="formBasicusername">
-              <Form.Label>User name</Form.Label>
-              <Form.Control type="user" placeholder="Enter Username" />
-            </Form.Group>
+const FormLogin = () => {
+	const [ formData, setFormData ] = useState({
+		username: '',
+		password: ''
+	});
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Remember me" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-            <p>
-              Forgot <a href="/recoverpassword">password</a> or{" "}
-              <a href="/recoverusername">username</a>?
-            </p>
-          </Form>
-        </Jumbotron>
-      </Container>
-    );
-  }
-}
+	const { username, password } = formData;
+
+	const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		console.log('Success');
+	};
+
+	return (
+		<Container>
+			<Jumbotron>
+				<h1 className="home-title">Welcome to TastyByte!</h1>
+				<h2>Login</h2>
+				<Form onSubmit={(e) => onSubmit(e)}>
+					<Form.Group controlId="formBasicusername">
+						<Form.Label>User name</Form.Label>
+						<Form.Control
+							type="user"
+							name="username"
+							value={username}
+							onChange={(e) => onChange(e)}
+							required
+							placeholder="Enter Username"
+						/>
+					</Form.Group>
+
+					<Form.Group controlId="formBasicPassword">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							name="password"
+							value={password}
+							onChange={(e) => onChange(e)}
+							required
+							minLength="6"
+							placeholder="Password"
+						/>
+					</Form.Group>
+					<Form.Group controlId="formBasicCheckbox">
+						<Form.Check type="checkbox" label="Remember me" />
+					</Form.Group>
+					<Button value="FormLogin" type="submit">
+						Submit
+					</Button>
+					<p>
+						Forgot <a href="/recoverpassword">password</a> or <a href="/recoverusername">username</a>?
+					</p>
+				</Form>
+			</Jumbotron>
+		</Container>
+	);
+};
 
 export default FormLogin;
